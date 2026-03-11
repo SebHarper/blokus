707abebcff25dbeeab996194913d302c5c683f28
Seb <sebharper22@gmail.com>
2026-03-11 21:18:38 +0000

    separate board state login from UI rendering

--- js/game.js
+++ js/game.js
@@ -1,25 +1,76 @@
-function addCells() {
-	
-	var board = $('#game');
+const rows = 20;
+const cols = 20;
 
-	for (let i=0; i < 400; i++ ) {
-		board.append('<div class="cell"></div>');
+const baseColor = "#bea9de"
+const selectedColor = "#cfbaef";
+
+let boardState;
+
+function initialiseBoard() {
+	boardState = [];
+	for (let r=0; r < rows; r++) {
+		boardState[r] = [];
+		for (let c=0; c < cols; c++) {
+			boardState[r][c] = {selected: false, value: 0}
+		}
+	}
+	return boardState;
+};
+
+function createCellElements(board) {
+	
+	var boardElement = $('#game');
+	boardElement.empty();
+	
+	for (let r=0; r < rows; r++) {
+		for (let c=0; c < cols; c++) {
+			boardElement.append(`<div class="cell" data-row=${r} data-col=${c}></div>`);
+		}
 	}
 };
 
-$(document).ready(function() {
-	addCells();
+function renderCell(row, col) {
+	let cellState = boardState[row][col];
 	
+	let cell = $(`.cell[data-row="${row}"][data-col="${col}"]`);
+	
+	if (cellState.selected) {
+		cell.css("background-color", selectedColor);
+	} else {
+		cell.css("background-color", baseColor);
+	}
+};
+
+function renderBoard() {
+	for (let r=0; r < rows; r++) {
+		for (let c=0; c < cols; c++) {
+			renderCell(r, c);
+		}
+	}
+};
+
+function handleCellClick(row, col) {
+	boardState[row][col].selected = !boardState[row][col].selected;	
+	
+	renderCell(row, col);
+};
+
+
+$(document).ready(function() {
+	boardState = initialiseBoard(rows, cols);
+	console.log(boardState);
+	
+	createCellElements();
+	renderBoard();
+
 	$(".cell").click(function() {
 
-		console.log("cell clicked")
-
 		var selector = $(this);
+				
+		let row = selector.data("row");
+		let col = selector.data("col");
 		
-		if (selector.hasClass("selected-cell")) {
-			selector.removeClass("selected-cell");
-		} else {
-			selector.addClass("selected-cell");
-		}
-	});	
+		handleCellClick(row, col);
+	});
+
 });
